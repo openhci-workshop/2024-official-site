70f4a61bffea64546423234a0a8a402a9b9319f3
tsaichiehhuang <a0970605512@gmail.com>
2024-05-07 20:30:40 +0800

    fix(banner):rwd, image position, no blur

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -10,52 +10,31 @@ export default function Banner() {
     const canvasRef = useRef<HTMLDivElement>(null)
     const TabletLayout = () => {
         return (
-            <div className="absolute z-20 hidden w-full h-screen grid-cols-12 text-right md:grid xl:hidden grid-rows-12">
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="	 tracking-widest col-end-11 col-span-12 row-start-5  md:text-[96px] leading-4 font-kompot text-white drop-shadow-3xl 	"
-                >
+            <div className="absolute z-20 hidden w-full h-screen grid-cols-12 text-right sm:grid xl:hidden grid-rows-12">
+                <div className="tracking-widest col-end-11 col-span-12 row-start-5  text-[96px] leading-4 font-kompot text-white drop-shadow-3xl 	">
                     RESILIENCE
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl ">
                     6
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl ">
                     22
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-10 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-10 font-kompot drop-shadow-3xl ">
                     6
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-10 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-10 font-kompot drop-shadow-3xl ">
                     23
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-8 col-end-11 row-start-6 text-5xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-8 col-end-11 row-start-6 text-5xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
                     24&apos; OPENHCI
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-8 col-end-11 row-start-7 text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-8 col-end-11 row-start-7 text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
                     第十四屆人機互動工作坊
                 </div>
                 <Button
                     variant="bordered"
-                    style={{ filter: 'blur(0.5px)', borderColor: '#F2D4CD' }}
+                    style={{ borderColor: '#F2D4CD' }}
                     className="col-span-2 col-end-11 text-xl leading-none tracking-widest text-white rounded-full shadow border-3 row-start-8 font-kompot drop-shadow-3xl backdrop-blur "
                 >
                     立即報名
@@ -66,51 +45,30 @@ export default function Banner() {
     const WebLayout = () => {
         return (
             <div className="absolute z-20 hidden w-full h-screen grid-cols-12 text-right xl:grid grid-rows-10">
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="	 tracking-widest col-end-11 col-span-12 row-start-5 text-[72px] md:text-[96px] leading-4 font-kompot text-white drop-shadow-3xl 	"
-                >
+                <div className="	 tracking-widest col-end-11 col-span-12 row-start-5 text-[72px] md:text-[96px] leading-4 font-kompot text-white drop-shadow-3xl 	">
                     RESILIENCE
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-8 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-8 font-kompot drop-shadow-3xl ">
                     6
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-8 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-8 font-kompot drop-shadow-3xl ">
                     22
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-6 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl ">
                     6
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-12 col-start-2 pt-12 pl-10 text-5xl leading-4 tracking-widest text-left text-white row-start-9 font-kompot drop-shadow-3xl ">
                     23
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-8 col-end-11 row-start-6 text-5xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-8 col-end-11 row-start-6 text-5xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
                     24&apos; OPENHCI
                 </div>
-                <div
-                    style={{ filter: 'blur(0.8px)' }}
-                    className="col-span-8 col-end-11 row-start-7 text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl "
-                >
+                <div className="col-span-8 col-end-11 row-start-7 text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
                     第十四屆人機互動工作坊
                 </div>
                 <Button
                     variant="bordered"
-                    style={{ filter: 'blur(0.5px)', borderColor: '#F2D4CD' }}
+                    style={{ borderColor: '#F2D4CD' }}
                     className="col-span-2 col-end-11 text-xl leading-none tracking-widest text-white rounded-full shadow border-3 row-start-8 font-kompot drop-shadow-3xl backdrop-blur "
                 >
                     立即報名
@@ -120,58 +78,76 @@ export default function Banner() {
     }
     const PhoneLayout = () => {
         return (
-            <div className="absolute z-20 flex flex-col w-full h-screen gap-32 px-10 py-64 text-right md:hidden ">
+            <div className="absolute z-20 flex flex-col w-full h-screen gap-32 px-10 py-64 text-right min-[500px]:hidden ">
                 <div className="flex flex-col items-center justify-center gap-4">
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="self-end text-6xl tracking-widest text-white font-kompot drop-shadow-3xl "
-                    >
+                    <div className="self-end text-6xl tracking-widest text-white font-kompot drop-shadow-3xl ">
                         RESILIENCE
                     </div>
 
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="self-end text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl "
-                    >
+                    <div className="self-end text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
                         24&apos; OPENHCI
                     </div>
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="self-end text-xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl "
-                    >
+                    <div className="self-end text-xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
                         第十四屆人機互動工作坊
                     </div>
                     <Button
                         variant="bordered"
-                        style={{ filter: 'blur(0.5px)', borderColor: '#F2D4CD' }}
+                        style={{ borderColor: '#F2D4CD' }}
                         className="self-end w-1/3 text-lg leading-none tracking-widest text-white rounded-full shadow border-3 font-kompot drop-shadow-3xl backdrop-blur "
                     >
                         立即報名
                     </Button>
                 </div>
                 <div className="flex flex-col items-start justify-start ">
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="pt-6 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl"
-                    >
+                    <div className="pt-6 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl">
                         6
                     </div>
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="pt-4 pl-10 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl"
-                    >
+                    <div className="pt-4 pl-10 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl">
                         22
                     </div>
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="pt-6 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl "
-                    >
+                    <div className="pt-6 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl ">
                         6
                     </div>
-                    <div
-                        style={{ filter: 'blur(0.8px)' }}
-                        className="pt-4 pl-10 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl"
+                    <div className="pt-4 pl-10 text-6xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl">
+                        23
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    const SmWindowLayout = () => {
+        return (
+            <div className="absolute z-20 flex-col w-full h-screen gap-10 px-10 text-right pt-28 min-[500px]:flex hidden min-[640px]:hidden">
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <div className="self-end text-6xl tracking-widest text-white font-kompot drop-shadow-3xl ">
+                        RESILIENCE
+                    </div>
+
+                    <div className="self-end text-3xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
+                        24&apos; OPENHCI
+                    </div>
+                    <div className="self-end text-xl leading-none tracking-widest text-white blur-lg font-kompot drop-shadow-3xl ">
+                        第十四屆人機互動工作坊
+                    </div>
+                    <Button
+                        variant="bordered"
+                        style={{ borderColor: '#F2D4CD' }}
+                        className="self-end w-1/3 text-lg leading-none tracking-widest text-white rounded-full shadow border-3 font-kompot drop-shadow-3xl backdrop-blur "
                     >
+                        立即報名
+                    </Button>
+                </div>
+                <div className="flex flex-col items-start justify-start ">
+                    <div className="pt-6 text-4xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl">
+                        6
+                    </div>
+                    <div className="pt-4 pl-8 text-4xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl">
+                        22
+                    </div>
+                    <div className="pt-6 text-4xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl ">
+                        6
+                    </div>
+                    <div className="pt-4 pl-8 text-4xl leading-3 tracking-widest text-left text-white font-kompot drop-shadow-3xl">
                         23
                     </div>
                 </div>
@@ -184,10 +160,11 @@ export default function Banner() {
                 <WebLayout />
                 <TabletLayout />
                 <PhoneLayout />
+                <SmWindowLayout />
                 {/* <div className="absolute z-10 w-full h-screen">
                     <Sketch canvasRef={canvasRef} />
                 </div> */}
-                <div className="w-full h-screen bg-[url('/GIF_Phone.gif')] md:bg-[url('/GIF_Tablet_直.gif')] xl:bg-[url('/GIF_Desktop.gif')] bg-cover brightness-90 z-0 absolute" />
+                <div className="w-full h-screen bg-bottom bg-[url('/GIF_Phone.gif')] sm:bg-[url('/GIF_Tablet_直.gif')] xl:bg-[url('/GIF_Desktop.gif')] bg-cover brightness-90 z-0 absolute" />
             </div>
             <div id="intro"></div>
         </>
