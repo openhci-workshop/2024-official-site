84c1bdc07ad0f79cacf40e8e66068abdfc0d99af
JackTsai004589 <jack89040589@yahoo.com.tw>
2024-05-08 14:40:34 +0800

    refactor: Update Student component to include random team selection on page load

--- src/app/components/student.tsx
+++ src/app/components/student.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 import { Button, ButtonGroup } from '@nextui-org/react'
@@ -13,6 +13,12 @@ export default function Student() {
     const handleButtonClick = (team: string) => {
         setSelectedTeam(team)
     }
+
+    useEffect(() => {
+        // choose the team randomly from the list of teams
+        const randomTeam = teams[Math.floor(Math.random() * teams.length)]
+        setSelectedTeam(randomTeam)
+    }, [])
 
     return (
         <div
@@ -45,17 +51,17 @@ export default function Student() {
                     ? students.map((student) => (
                           <div key={student.id} className="flex flex-col items-stretch col-span-3 xl:col-span-1">
                               <Image
-                                  width={300}
-                                  height={300}
+                                  width={245}
+                                  height={245}
                                   // className='w-[185px] h-[185px]  xl:w-[245px] xl:h-[245px] rounded-lg self-center mb-3'
                                   className="self-start object-cover w-full mb-3 bg-cover rounded-lg h-1/3"
                                   src={student.image}
                                   alt={student.name}
                               />
                               <div className="flex flex-col gap-1">
-                                  <span className="text-xs font-medium xl:text-xl">姓名 {student.name}</span>
+                                  <span className="text-xs font-medium xl:text-xl">{student.name}</span>
                                   <span className="text-xxs xl:text-base">
-                                      學校 科系 {student.school} {student.department}
+                                    {student.school} {student.department}
                                   </span>
                               </div>
                           </div>
@@ -65,17 +71,17 @@ export default function Student() {
                           .map((student) => (
                               <div key={student.id} className="flex flex-col items-stretch justify-start col-span-1">
                                   <Image
-                                      width={300}
-                                      height={300}
+                                      width={245}
+                                      height={245}
                                       // className='w-[185px] h-[185px]  xl:w-[245px] xl:h-[245px] rounded-lg self-center mb-3'
                                       className="self-start object-cover w-full mb-3 bg-cover rounded-lg h-1/2"
                                       src={student.image}
                                       alt={student.name}
                                   />
                                   <div className="flex flex-col gap-1">
-                                      <span className="text-xs font-medium xl:text-xl">姓名 {student.name}</span>
+                                      <span className="text-xs font-medium xl:text-xl">{student.name}</span>
                                       <span className="text-xxs xl:text-base">
-                                          學校 科系 {student.school} {student.department}
+                                        {student.school} {student.department}
                                       </span>
                                   </div>
                               </div>
--- src/app/components/studentAndTeam.ts
+++ src/app/components/studentAndTeam.ts
@@ -1,4 +1,6 @@
-export const teams = [
+type Teams = string[];
+
+export const teams: Teams = [
     "總召",
     "行銷",
     "人資",
@@ -14,109 +16,350 @@ export const teams = [
     "技術 TA",
 ]
 
-export const students = [
+
+type Students = {
+    team: string; 
+    name: string; 
+    school: string; 
+    department: string;
+    image?: string;
+};
+
+export const students: Students[] = [
     {
-        id: 1,
-        team: '總召',
-        name: 'A Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "總召",
+        "name": "張恩愷",
+        "school": "國立陽明交通大學",
+        "department": "傳播與科技碩士班",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 2,
-        team: '行銷',
-        name: 'B Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "總召",
+        "name": "陳玟諭",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 3,
-        team: '人資',
-        name: 'C Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "總召",
+        "name": "賴詠晴",
+        "school": "國立臺灣大學",
+        "department": "資訊工程學系",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 4,
-        team: '財務',
-        name: 'D Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "行銷",
+        "name": "楊芷嫺",
+        "school": "國立陽明交通大學",
+        "department": "百川學士學位學程",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 5,
-        team: '視覺',
-        name: 'E Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "行銷",
+        "name": "王雅靜",
+        "school": "國立陽明交通大學",
+        "department": "傳播與科技碩士班",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 6,
-        team: '網管',
-        name: 'F Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "行銷",
+        "name": "盧亭臻",
+        "school": "國立臺灣大學",
+        "department": "圖書資訊學系",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 7,
-        team: '公關',
-        name: 'G Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "人資",
+        "name": "揭宜蓁",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 8,
-        team: '演講',
-        name: 'H Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "人資",
+        "name": "廖語岑",
+        "school": "國立政治大學",
+        "department": "心理學系",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 9,
-        team: '攝影',
-        name: 'I Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "人資",
+        "name": "王立蓁",
+        "school": "國立政治大學",
+        "department": "歷史學系/數位內容學士學位學程",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 10,
-        team: '技術教學',
-        name: 'J Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "財務",
+        "name": "曾苡涵",
+        "school": "大同大學",
+        "department": "事業經營系",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 11,
-        team: '設計教學',
-        name: 'K Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "財務",
+        "name": "曾敬哲",
+        "school": "國立政治大學",
+        "department": "金融所",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 12,
-        team: '設計 TA',
-        name: 'L Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "視覺",
+        "name": "葉聿昕",
+        "school": "國立清華大學",
+        "department": "經濟學系",
+        "image": "/images/album-cover.png"
     },
     {
-        id: 13,
-        team: '技術 TA',
-        name: 'M Doe',
-        school: 'ABC School',
-        department: 'Computer Science',
-        image: '/images/album-cover.png',
+        "team": "視覺",
+        "name": "孫若晏",
+        "school": "國立中山大學",
+        "department": "資訊管理學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "視覺",
+        "name": "王芷洺",
+        "school": "國立政治大學",
+        "department": "傳播碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "視覺",
+        "name": "陳媛婷",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "視覺",
+        "name": "張致瑜",
+        "school": "國立台北科技大學",
+        "department": "互動設計所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "網管",
+        "name": "蔡明達",
+        "school": "國立臺灣科技大學",
+        "department": "智慧製造科技研究所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "網管",
+        "name": "童嬿瑜",
+        "school": "國立臺灣大學",
+        "department": "資訊工程學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "網管",
+        "name": "黃采婕",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "公關",
+        "name": "張以柔",
+        "school": "國立臺灣大學",
+        "department": "圖書資訊學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "公關",
+        "name": "吳安倢",
+        "school": "國立中山大學",
+        "department": "資訊管理所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "公關",
+        "name": "彭鈺婷",
+        "school": "國立清華大學",
+        "department": "藝術學院學士班",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "公關",
+        "name": "黃榆禎",
+        "school": "國立陽明交通大學",
+        "department": "傳播與科技學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "演講",
+        "name": "陳育陞",
+        "school": "國立政治大學",
+        "department": "數位內容學士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "演講",
+        "name": "王士涵",
+        "school": "國立臺灣大學",
+        "department": "外國語文學系/心理學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "演講",
+        "name": "陳品頤",
+        "school": "國立臺灣大學",
+        "department": "工商管理學系 科技管理組",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "攝影",
+        "name": "李聿屏",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術教學",
+        "name": "方群鈞",
+        "school": "國立臺灣大學",
+        "department": "資訊工程學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術教學",
+        "name": "林冠銘",
+        "school": "國立成功大學",
+        "department": "電機系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術教學",
+        "name": "陳士茵",
+        "school": "國立中正大學",
+        "department": "資訊工程所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計教學",
+        "name": "黃宣銘",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計教學",
+        "name": "陳玉書",
+        "school": "國立臺灣科技大學",
+        "department": "設計所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計教學",
+        "name": "楊詠晴",
+        "school": "國立臺灣大學",
+        "department": "心理學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "黃鈺善",
+        "school": "國立臺灣科技大學",
+        "department": "設計所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "紀玟伶",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "廖奕慈",
+        "school": "國立台北科技大學",
+        "department": "互動設計所",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "李佳蓁",
+        "school": "國立臺灣大學",
+        "department": "圖書資訊學系雙主修創新領域學士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "賴竹原",
+        "school": "國立臺灣大學",
+        "department": "創新領域學士學位",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "許青純",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "設計 TA",
+        "name": "楊祐維",
+        "school": "國立台灣大學",
+        "department": "創新領域學士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "王新翌",
+        "school": "國立臺灣科技大學",
+        "department": "資訊工程學系/互動設計學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "胡芸毓",
+        "school": "國立政治大學",
+        "department": "數位內容碩士學位學程",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "陳以潼",
+        "school": "國立臺灣大學",
+        "department": "資訊管理學系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "蔡宜珊",
+        "school": "陽明交通大學",
+        "department": "電機系",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "林珀鋒",
+        "school": "臺灣科技大學",
+        "department": "資訊工程碩士班",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "王聖翔",
+        "school": "國立臺灣科技大學",
+        "department": "資訊管理碩士班",
+        "image": "/images/album-cover.png"
+    },
+    {
+        "team": "技術 TA",
+        "name": "吳易陞",
+        "school": "雲林科技大學",
+        "department": "資訊管理學系",
+        "image": "/images/album-cover.png"
     }
 ]
+
+// export const studentsWithDynamicImage = students.map(student => {
+//     return {
+//         ...student,
+//         image: `/images/OPENHCI形象照_${student.name}.JPG`
+//     }
+// })
