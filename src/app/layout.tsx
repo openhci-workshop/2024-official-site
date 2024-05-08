06272d5377b8d4e34f5035383b43f2c59c603b3f
JackTsai004589 <jack89040589@yahoo.com.tw>
2024-05-08 16:19:27 +0800

    refactor: Update metadata and add OpenHCI 2024 keywords in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,33 +7,47 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 const notoSansTC = Noto_Sans_TC({ subsets: ['latin'], weight: ['100', '400', '500', '600', '700', '800', '900'] })
 
 // Define the environment variable
-// const GOOGLE_TAG_MANAGER = process.env.GOOGLE_TAG_MANAGER;
-// const GOOGLE_ANALYTICS = process.env.GOOGLE_ANALYTICS;
+const GOOGLE_TAG_MANAGER = process.env.GOOGLE_TAG_MANAGER;
+const GOOGLE_ANALYTICS = process.env.GOOGLE_ANALYTICS;
 
 export const metadata: Metadata = {
     title: 'OpenHCI 2024',
     description: 'OpenHCI 2024 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
     metadataBase: new URL('https://www.2024.openhci.com/'),
+    keywords: ['OpenHCI', '2024', '垮領域', '人機互動'],
+    // alternates: {
+    //     canonical: '/',
+    //     languages: {
+    //         'en-US': '/en-US',
+    //         'zh-Hant': '/zh-Hant',
+    //     },
+    // },
     alternates: {
-        canonical: '/',
+        canonical: 'https://www.2024.openhci.com/',
         languages: {
-            'en-US': '/en-US',
-            'zh-Hant': '/zh-Hant',
+            'en-US': 'https://www.2024.openhci.com/',
+            'zh-Hant': 'https://www.2024.openhci.com/',
         },
     },
     openGraph: {
+        title: 'OpenHCI 2024',
+        description: 'OpenHCI 2024 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
+        url: 'https://www.2024.openhci.com/',
         images: '/opengraph-image.png',
+        siteName: 'OpenHCI 2024',
+        type: 'website',
     },
     icons: {
         icon: '/icon.svg',
-    },
+    },    
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <GoogleTagManager gtmId={GOOGLE_TAG_MANAGER || 'default'} />
             <body className={notoSansTC.className}>{children}</body>
-            {/* <GoogleAnalytics gaId={GOOGLE_ANALYTICS || 'default'} /> */}
+            <GoogleAnalytics gaId={GOOGLE_ANALYTICS || 'default'} />
         </html>
     )
 }
