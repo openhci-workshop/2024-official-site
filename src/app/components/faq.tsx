43b908d6b5e5ae7d991867de51fc898ede638366
Jack Da Tsai <web@openhci.com>
2024-05-09 02:21:10 +0800

    chore: Optimize image loading in FAQ component

--- src/app/components/faq.tsx
+++ src/app/components/faq.tsx
@@ -14,13 +14,15 @@ export default function FAQ() {
                 <span className="mb-5 font-semibold text-md xl:text-3xl md:text-2xl">FAQ</span>
                 <span className="mb-5 text-sm font-medium xl:text-2xl md:text-xl">報名常見問題</span>
                 <div className="flex items-start justify-center w-full mb-5" data-aos="fade-right">
-                    <Image src={FaqM1} alt="faq_m_q1" className="md:hidden " width={1000} height={1000} />
-                    <Image src={FaqD1} alt="faq_d_q1" className="hidden md:block " width={1000} height={1000} />
+                    <Image src={FaqM1} alt="faq_m_q1" className="blcok sm:hidden" width={500} height={500} />
+                    <Image src={FaqD1} alt="faq_d_q1" className="hidden sm:block lg:hidden" width={750} height={750} />
+                    <Image src={FaqD1} alt="faq_d_q1" className="hidden lg:block " width={1000} height={1000} />
                 </div>
                 <h3 className="mb-5 text-sm font-medium xl:text-2xl md:text-xl">ID Card 常見問題</h3>
                 <div className="flex justify-center w-full" data-aos="fade-left">
-                    <Image src={FaqM4} alt="faq_m_q1" className="md:hidden " width={1000} height={1000} />
-                    <Image src={FaqD4} alt="faq_d_q4" className="hidden md:block" width={1000} height={1000} />
+                    <Image src={FaqM4} alt="faq_m_q4" className="block sm:hidden" width={500} height={500} />
+                    <Image src={FaqD4} alt="faq_d_q4" className="hidden sm:block lg:hidden" width={750} height={750} />
+                    <Image src={FaqD4} alt="faq_d_q4" className="hidden lg:block " width={1000} height={1000} />
                 </div>
             </div>
             <div id="works"></div>
