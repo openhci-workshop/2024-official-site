e66035733f80eb53ebd86b170184c4c05595b187
JackTsai004589 <jack89040589@yahoo.com.tw>
2024-05-09 20:52:48 +0800

    refactor: Update metadata keywords in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
         'OpenCHI2024', 'OpenCHI 2024', 'Open CHI 2024', 'Open CHI2024',
         '2024OpenHCI', '2024 OpenHCI', '2024 Open HCI', 
         '2024 OpenCHI', '2024 OpenCHI2024', '2024 Open CHI', 
-        'HCI', 'CHI', 'Human Computer Interaction', 'Computer-Human Interaction', 'Conference on Human Factors in Computing Systems', 
+        'HCI', 'CHI', 'Human-Computer Interaction', 'Computer-Human Interaction', 'Conference on Human Factors in Computing Systems', 
         '人機互動', '工作坊', '人機互動工作坊',
         'OpenHCI 2024 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
     ],
