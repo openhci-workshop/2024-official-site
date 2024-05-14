22d5825278d89ea73997f40eea5f7e0ee025403c
JackTsai004589 <jack89040589@yahoo.com.tw>
2024-05-14 20:57:09 +0800

    refactor: Remove commented out code and update sponsor image in Sponsor component

--- src/app/components/sponsor.tsx
+++ src/app/components/sponsor.tsx
@@ -29,13 +29,6 @@ export default function Sponsor() {
 
                 {/* Image */}
                 <Image
-                    src={NYCU_Logo}
-                    width={300}
-                    height={300}
-                    alt="NYCU_Logo"
-                    className="rounded-md col-span-3 md:col-span-2 bg-white aspect-[2/1] object-contain"
-                />
-                <Image
                     src={ITSA_Logo}
                     width={300}
                     height={300}
@@ -54,6 +47,13 @@ export default function Sponsor() {
                 /> */}
 
                 {/* Image */}
+                <Image
+                    src={NYCU_Logo}
+                    width={300}
+                    height={300}
+                    alt="NYCU_Logo"
+                    className="rounded-md col-span-3 md:col-span-2 bg-white aspect-[2/1] object-contain"
+                />                
             </div>
             <span className="text-xs font-semibold xl:text-xl">銀級</span>
             <div className="grid grid-cols-6 gap-2 sm:gap-4 md:gap-6 lg:gap-8 xl:gap-10 mb-5 w-full ">
